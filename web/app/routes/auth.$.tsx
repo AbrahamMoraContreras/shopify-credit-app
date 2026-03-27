d2ea145a514c53321815cb60d85168e06be94e8c
abraham <abrahampruebasespeciales@gmail.com>
2026-03-27 02:00:49 -0400

    fix: auth

---
+++ web/app/routes/auth.$.tsx
@@ -0,0 +1,12 @@
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "react-router";
+import { authenticate } from "../../shopify.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  await authenticate.admin(request);
+  return null;
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  await authenticate.admin(request);
+  return null;
+};
